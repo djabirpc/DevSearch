b861d9e0b6f0432e4026e3e49fed0bb4fde0b8ee
djabirpc <djabir.pc@outlook.fr>
2021-09-24 22:39:56 +0100

    User , Skill CRUD

--- users/urls.py
+++ users/urls.py
@@ -7,7 +7,18 @@ urlpatterns = [
     path('logout/', views.logoutUser, name="logout"),
     path('register/', views.registerUser, name="register"),
 
+    #User Account
+    path('account/', views.userAccount, name="account"),
+    path('edit-account/', views.editAccount, name="edit-account"),
+
     #Profile
     path('', views.profiles, name="profiles"),
     path('profile/<str:pk>/', views.userProfile, name="user-profile"),
+
+    #Skill
+    path('create-skill/', views.createSkill, name="create-skill"),
+    path('update-skill/<str:pk>/', views.updateSkill, name="update-skill"),
+    path('delete-skill/<str:pk>/', views.deleteSkill, name="delete-skill"),
+
+
 ]
