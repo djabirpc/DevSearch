4f818ec505b43113290eba40b45e842387dce00a
djabirpc <djabir.pc@outlook.fr>
2021-09-25 22:22:41 +0100

    Reviews & Messages

--- projects/models.py
+++ projects/models.py
@@ -26,7 +26,23 @@ class Project(models.Model):
         return self.title
     
     class Meta:
-        ordering = ['-created']
+        ordering = ['-vote_ratio','-vote_total','title']
+
+    @property
+    def reviewers(self):
+        queryset = self.review_set.all().values_list('owner__id',flat=True)
+        return queryset
+    
+    @property
+    def getVoteCount(self):
+        reviews = self.review_set.all()
+        upVotes = reviews.filter(value='up').count()
+        totalVotes =reviews.count()
+
+        ration = (upVotes/ totalVotes) * 100
+        self.vote_total = totalVotes
+        self.vote_ratio = ration
+        self.save()
 
 
 class Review(models.Model):
@@ -42,6 +58,8 @@ class Review(models.Model):
     id = models.UUIDField(default=uuid.uuid4, unique=True,
                           primary_key=True, editable=False)
 
+    class Meta:
+        unique_together = [['owner','project']]
 
     def __str__(self):
         return self.value
--- projects/views.py
+++ projects/views.py
@@ -1,9 +1,10 @@
 
 from django.core import paginator
 from django.shortcuts import render, redirect
+from django.contrib import messages
 from django.contrib.auth.decorators import login_required
 from .models import Project
-from .forms import ProjectForm
+from .forms import ProjectForm,ReviewFrom
 from .utils import searchProject,paginationProject
 
 
@@ -23,8 +24,19 @@ def projects(request):
 
 def project(request, pk):
     projectObj = Project.objects.get(id=pk)
+    form = ReviewFrom()
 
-    return render(request, 'projects/single-project.html', {'project': projectObj})
+    if request.method == 'POST':
+        form = ReviewFrom(request.POST)
+        review = form.save(commit= False)
+        review.project = projectObj
+        review.owner = request.user.profile
+        review.save()
+
+        projectObj.getVoteCount
+        messages.success(request,'Review Added!')
+        return redirect('project',pk = projectObj.id)
+    return render(request, 'projects/single-project.html', {'project': projectObj,'form':form})
 
 @login_required(login_url='login')
 def createProject(request):
