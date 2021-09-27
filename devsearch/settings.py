4404816978dd202c0b0f6db0e009d39ccb2461c7
djabirpc <djabir.pc@outlook.fr>
2021-09-27 02:11:24 +0100

    To Postgresql

--- devsearch/settings.py
+++ devsearch/settings.py
@@ -126,10 +126,21 @@ WSGI_APPLICATION = 'devsearch.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'devsearch',
+        'USER': 'postgres',
+        'PASSWORD': 'papamama123',
+        'HOST': 'localhost',
+        'PORT': '5432'
     }
 }
 
